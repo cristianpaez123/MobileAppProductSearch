0cd8cda2e8be6381e0765ebbea05f16ca604d521
Cristian Paez <cristianpaezguerrero@gmail.com>
2025-05-22 09:17:34 -0500

    configure Hilt and add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+
+
 }
 
 android {
@@ -45,4 +49,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt ("com.google.dagger:hilt-compiler:2.48")
+
+
 }
---
+++ app/src/main/java/com/example/mobileappproductsearch/ProductSearchApp.kt
@@ -0,0 +1,7 @@
+package com.example.mobileappproductsearch
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ProductSearchApp {
+}
