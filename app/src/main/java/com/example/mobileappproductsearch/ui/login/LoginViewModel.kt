150719ab17ad7ddc80f40e8e6e0d55cfc51c7a25
Cristian Paez <cristianpaezguerrero@gmail.com>
2025-05-27 19:58:27 -0500

    add BaseViewModel to LoginViewModel

--- app/src/main/java/com/example/mobileappproductsearch/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/mobileappproductsearch/ui/login/LoginViewModel.kt
@@ -1,47 +1,57 @@
 package com.example.mobileappproductsearch.ui.login
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.mobileappproductsearch.R
 import com.example.mobileappproductsearch.domain.model.LoginResult
 import com.example.mobileappproductsearch.domain.useCase.LoginUseCase
+import com.example.mobileappproductsearch.ui.common.BaseViewModel
 import com.example.mobileappproductsearch.utils.CredentialValidator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase
-) : ViewModel() {
+) : BaseViewModel() {
 
     private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Idle)
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
-
     fun login(email: String, password: String) {
-        val errorResId = CredentialValidator.validate(email, password)
-        if (errorResId != null) {
-            _uiState.value = LoginUiState.Error.MessageRes(resId = errorResId)
-            return
-        }
+        if (!isInputValid(email, password)) return
+
         _uiState.value = LoginUiState.Loading
-        viewModelScope.launch {
-            try {
-                when (val result = loginUseCase(email, password)) {
-                    is LoginResult.Success -> _uiState.value = LoginUiState.Success
 
-                    is LoginResult.Failure ->
-                        _uiState.value = LoginUiState.Error.MessageRes(result.errorResId)
-
-                }
-            } catch (e: Exception) {
-                _uiState.value = LoginUiState.Error.MessageRes(R.string.error_unexpected)
-            }
+        launch(onError = ::handleLoginError) {
+            val result = loginUseCase(email, password)
+            handleLoginResult(result)
         }
     }
 
+    private fun isInputValid(email: String, password: String): Boolean {
+        val errorResId = CredentialValidator.validate(email, password)
+        return if (errorResId != null) {
+            _uiState.value = LoginUiState.Error.MessageRes(errorResId)
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun handleLoginResult(result: LoginResult) {
+        _uiState.value = when (result) {
+            is LoginResult.Success -> LoginUiState.Success
+            is LoginResult.Failure -> LoginUiState.Error.MessageRes(result.errorResId)
+        }
+    }
+
+    private fun handleLoginError(throwable: Throwable) {
+        val messageUiState = throwable.message?.let {
+            LoginUiState.Error.MessageText(it)
+        } ?: LoginUiState.Error.MessageRes(R.string.error_unexpected)
+
+        _uiState.value = messageUiState
+    }
 }
